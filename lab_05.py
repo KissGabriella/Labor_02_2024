55a432903e64d9c0907ffb476f42f00b194eeace
KissGabriella <kissgabriella0813@gmail.com>
2024-10-10 12:38:43 +0200

    Szinkron 6

--- lab_05.py
+++ lab_05.py
@@ -2,18 +2,6 @@
 from lab_05_modul import *
 
 
-def beleptetes():
-    print("Beléptetés")
-
-
-def regisztracio():
-    print("Regisztráció")
-
-
-#Főprogram
+# főprogram
 #regisztracio()
-#beleptetes()
-felhasznalo_neve = felhasznalonev()
-print(felhasznalo_neve)
-felhasznalo_jelszo = jelszo_bekerese(10)
-print(felhasznalo_jelszo)
+beleptetes()
